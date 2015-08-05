7f5de044dffbe6e77e4e24a45cb0b81e9ed7504a
Andrew D. Bagdanov <bagdanov@gmail.com>
2015-08-05 11:28:12 +0200

    make script executable.

--- color_name_recognizer/train_recognizer.py
+++ color_name_recognizer/train_recognizer.py
@@ -1,13 +1,38 @@
-import os
-import utils
-import arguments
-from dataset import Dataset
+#!/usr/bin/env python
+#
+# This is a simple script that instantiates, trains and serializes a
+# colorname masked object recognizer on the images in the provided
+# dataset directory.
+#
+# Run it like this:
+#
+#  ./train_recognizer.py --n_jobs=4 --patch_size=10 --stride=5  dataset_jordi/
+#
+# The trained model is serialized to the <datadir>/models directory.
+#
+# See:
+#
+#  CNRecognizer.py - for the recognizer logic itself.
+#  dataset.py - for the expected layout of the <datadir> directory.
+#
+
+import argparse
 from CNRecognizer import CNRecognizer
-# from PythonCN import extract_all_features
-from NBNN import NBNN
+
+# Setup argument parser.
+parser = argparse.ArgumentParser(description='Train CNRecognizer on training dataset.')
+parser.add_argument('--patch_size', metavar='patch_size', type=int,
+                    default=10, help='size of local patches')
+parser.add_argument('--n_jobs', metavar='n_jobs', type=int,
+                    default=10, help='number of parallel jobs for feature extraction')
+parser.add_argument('--stride', metavar='stride', type=int,
+                    default=5,
+                    help='stride of sliding window for patch extraction')
+parser.add_argument('datadir', metavar='datadir', type=str,
+                    help='dataset directory')
 
 # Parse command line arguments.
-args = arguments.args
+args = parser.parse_args()
 
 # Train model (caching is done automatically).
 print 'Training and serializing model...'
