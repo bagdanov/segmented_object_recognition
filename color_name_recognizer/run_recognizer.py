91fe543fc4635fe83395220acff2e6fe20fb766f
Andrew D. Bagdanov <bagdanov@gmail.com>
2015-08-05 08:23:26 +0200

    working run_recognizer.py with timing

--- color_name_recognizer/run_recognizer.py
+++ color_name_recognizer/run_recognizer.py
@@ -1,7 +1,7 @@
 import sys
 import argparse
-import num
 import pylab as pl
+from utils import Timer
 from CNRecognizer import CNRecognizer
 from PythonCN import extract_all_image_features
 from NBNN import NBNN
@@ -18,18 +18,17 @@ mask = pl.imread('rubiks_asus_test_mask.png')
 
 # Instantiate (and deserialize, if already trained) recognizer.
 clf = CNRecognizer(args.datadir)
-#clf.train(10, 5, 4)
 if not clf._trained:
     print 'CNRecognizer in {} not trained! See train_recognizer.py script.'
     sys.exit(1)
 
-# Extract masked features from all images.
-#(Xtr, ytr) = extract_all_image_features(clf._dataset,
-#                                        n_jobs=4,
-#                                        patch_size=10,
-#                                        stride=5)
-
 # And classify.
-pred = clf.predict(image, mask)
+with Timer('time to extract and classify'):
+    pred = clf.predict(image, mask)
 
+# Use the '_dataset' attribute of the recognizer to access classes.
+print 'Class probabilities:'
+for (label, p) in enumerate(pred):
+    print '  {0:.2f}: {1:s}'.format(p, clf._dataset.label2class(label))
+print '\nPrediction: {}'.format(clf._dataset.label2class(pl.argmax(pred)))
 
