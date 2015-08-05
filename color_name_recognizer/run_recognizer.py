7d548e980c6bf6f62f92f46cca18a24046f2ce13
Andrew D. Bagdanov <bagdanov@gmail.com>
2015-08-05 11:48:25 +0200

    made executable and documented

--- color_name_recognizer/run_recognizer.py
+++ color_name_recognizer/run_recognizer.py
@@ -1,10 +1,28 @@
+#!/usr/bin/env python
+#
+# This is a very simple script that shows how to run a trained
+# classifier on test images. Run it like this:
+#
+#  ./run_recognizer.py dataset_jordi/
+#
+# The script will instantiate and deserialize a trained classifier
+# from the provided <datadir> directory, then use it to predict the
+# class of a test image (given by the 'rubiks_asus_test_image.png' and
+# 'rubiks_asus_test_mask.png' images). It prints some timing and
+# prediction information.
+#
+# See:
+#  train_recognizer.py - for a script that trains a recognizer on a dataset.
+#
+# TODO: Make the timing information more statistically meaningful.
+# TODO: Switch to using PIL for image I/O.
+#
+
 import sys
 import argparse
 import pylab as pl
 from utils import Timer
 from CNRecognizer import CNRecognizer
-from PythonCN import extract_all_image_features
-from NBNN import NBNN
 
 # Parse command line arguments.
 parser = argparse.ArgumentParser(description='Example of how to run trained CNRecognizer.')
@@ -12,7 +30,9 @@ parser.add_argument('datadir', metavar='datadir', type=str,
                     help='dataset directory')
 args = parser.parse_args()
 
-# Read images.
+# Read images. NOTE WELL: image channels *MUST* be between 0 and
+# 255. For some goddamn reason pylab scales PNGs to [0,1], but not
+# JPEGs. Thanks. Anyway, be careful.
 image = pl.imread('rubiks_asus_test_image.png') * 255.0
 mask = pl.imread('rubiks_asus_test_mask.png')
 
